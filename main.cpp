1d8916e59416b74cee2d7cfbbcdfbf697e7d3f62
Daniel Lengyel <daniel.lengyel@berkeley.edu>
2019-04-01 10:02:11 -0700

    Made a cylinder graph object

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Daniel Lengyel on 2019-04-01.
+//
+#include <iostream>
+#include "Graph.h"
+
+using namespace std;
+
+int main(){
+    CylinderGraph graph(2, 3);
+    graph.initializeGraph();
+    std::cout << "Hey";
+    return 0;
+}
