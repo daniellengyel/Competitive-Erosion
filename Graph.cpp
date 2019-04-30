b922c4d6c7e05f482443ec6cad87327f9c9689a5
Daniel Lengyel <daniel.lengyel@berkeley.edu>
2019-04-29 22:40:39 -0700

    added shady arbitrary graph

--- Graph.cpp
+++ Graph.cpp
@@ -230,35 +230,213 @@ void CylinderGraph::printGraph() {
   std::cout << std::endl;
 }
 
-int main() {
-  std::cout << "starting...." << std::endl;
-  auto start = std::chrono::steady_clock::now();
+int oldmain() {
+    std::cout << "starting...." << std::endl;
+    auto start = std::chrono::steady_clock::now();
 
-  CylinderGraph graph(1024, 1024);
-  graph.initializeGraph();
-  graph.MarkovChain(100, 300000);
-  ofstream myfile;
-  myfile.open ("example.txt");
+    CylinderGraph graph(1024, 1024);
+    graph.initializeGraph();
+    graph.MarkovChain(100, 300000);
+    ofstream myfile;
+    myfile.open ("example.txt");
 
-  for (auto x : graph.fluctuations) {
-    cout << x << ", ";
-  }
-  std::cout << std::endl;
+    for (auto x : graph.fluctuations) {
+        cout << x << ", ";
+    }
+    std::cout << std::endl;
 
-  for (auto c : graph.correlations) {
-    cout << c;
-    cout << ", ";
-  }
+    for (auto c : graph.correlations) {
+        cout << c;
+        cout << ", ";
+    }
 
-  myfile.close();
-  std::cout << std::endl;
-  graph.printDensityGraph(2);
+    myfile.close();
+    std::cout << std::endl;
+    graph.printDensityGraph(2);
 
-  auto end = std::chrono::steady_clock::now();
-  auto diff = end - start;
-  typedef std::chrono::duration<float> float_seconds;
-  auto secs = std::chrono::duration_cast<float_seconds>(diff);
-  std::cout << "Time elapsed: " << secs.count() << " s." << std::endl;
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+    typedef std::chrono::duration<float> float_seconds;
+    auto secs = std::chrono::duration_cast<float_seconds>(diff);
+    std::cout << "Time elapsed: " << secs.count() << " s." << std::endl;
 
-  return 0;
+    return 0;
 }
+
+
+ArbitraryGraph::ArbitraryGraph(int N, int num_colors, vector<vector<vector<int>>> starting_regions,
+                               function<int(double, double)> domain_function) {
+    ArbitraryGraph::N = N;
+    ArbitraryGraph::num_colors = num_colors;
+    ArbitraryGraph::starting_regions = starting_regions;
+    ArbitraryGraph::domain_function = domain_function;
+}
+
+void ArbitraryGraph::initializeGraph() {
+    graph = vector<int>(N * N, NO_COLOR);
+
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(0, num_colors - 1);
+
+  for (int y = 0; y < N; y++) {
+    for (int x = 0; x < N; x++) {
+        if(domain_function(1.0*x/N, 1.0*y/N)){
+            setColor(x, y, dist(mt));
+        }
+    }
+  }
+}
+
+int ArbitraryGraph::getColor(int x, int y) {
+    if((x < 0) or (x >= N)){
+        return NO_COLOR;
+    }
+    if((y < 0)  or (y >= N)){
+        return NO_COLOR;
+    }
+    return graph[y * N  + x];
+}
+
+
+
+void ArbitraryGraph::setColor(int x, int y, int color) {
+    if((x < 0) or (x >= N)){
+        return;
+    }
+    if((y < 0)  or (y >= N)){
+        return;
+    }
+    graph[y * N + x] = color;
+}
+
+std::vector<int> ArbitraryGraph::randomWalk(
+        int startX, int startY, int startColor,
+        std::uniform_real_distribution<double>& dist, std::mt19937& mt) {
+    int x = startX;
+    int y = startY;
+
+    char next_neighbor;
+    double rand_neighbor;
+    while (getColor(x, y) == startColor) {
+        rand_neighbor = dist(mt);
+        vector<char> valid_neighbors;
+        if(getColor(x + 1, y) != NO_COLOR){
+            valid_neighbors.push_back('R');
+        }
+        if(getColor(x - 1, y) != NO_COLOR){
+            valid_neighbors.push_back('L');
+        }
+        if(getColor(x, y+1) != NO_COLOR){
+            valid_neighbors.push_back('U');
+        }
+        if(getColor(x, y-1) != NO_COLOR){
+            valid_neighbors.push_back('D');
+        }
+
+        for(int i = 1; i < (valid_neighbors.size() + 1); i++){
+            if(rand_neighbor < i/(1.0*valid_neighbors.size()) ){
+                next_neighbor = valid_neighbors[i-1];
+                break;
+            }
+        }
+        // TODO what if no valid neighbor found?
+
+        if (next_neighbor == 'U') {
+            y++;
+        }
+        if (next_neighbor == 'D') {
+            y--;
+        }
+        if (next_neighbor == 'R') {
+            x++;
+        }
+        if (next_neighbor == 'L') {
+            x--;
+        }
+    }
+    return {x, y};
+}
+
+void ArbitraryGraph::MarkovChain(int num_samples) {
+
+    std::random_device rdRW;
+    std::mt19937 mtRW(rdRW());
+    std::uniform_real_distribution<double> distRW(0, 1);
+
+    int k;
+    vector<int> p;
+    vector<int> dest;  // TODO make point
+
+    for (int sample = 0; sample < num_samples; sample++) {
+
+        for (int color = 0; color < num_colors; color++) {
+
+            std::random_device rd;
+            std::mt19937 mt(rd());
+            std::uniform_int_distribution<int> dist(0, starting_regions[color].size() - 1);
+
+            k =  dist(mt);
+            p = starting_regions[color][k];
+            dest = randomWalk(p[0], p[1], color, distRW, mtRW);
+            setColor(dest[0], dest[1], color);
+        }
+
+        if(sample % 100 == 0){
+            printGraph();
+        }
+
+    }
+}
+
+
+void ArbitraryGraph::printGraph() {
+    for (int y = N - 1 ; y >= 0; y--) {
+        for (int x = 0; x < N; x++) {
+            int c = getColor(x, y);
+            std::cout << c;
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int always_true(int x, int y){
+    return 1;
+}
+
+int main() {
+    std::cout << "starting...." << std::endl;
+    auto start = std::chrono::steady_clock::now();
+
+    vector<vector<vector<int>>> startin_pos  = {{{0,0}}, {{9, 9}}}; // is starting pos on domain or lattice? Should be domain.  Fix it!
+    ArbitraryGraph graph(100, 2, startin_pos, always_true);
+    graph.initializeGraph();
+    graph.printGraph();
+    graph.MarkovChain(1000000);
+//    ofstream myfile;
+//    myfile.open ("example.txt");
+//
+//    for (auto x : graph.fluctuations) {
+//        cout << x << ", ";
+//    }
+//    std::cout << std::endl;
+//
+//    for (auto c : graph.correlations) {
+//        cout << c;
+//        cout << ", ";
+//    }
+//
+//    myfile.close();
+//    std::cout << std::endl;
+//    graph.printDensityGraph(2);
+//
+//    auto end = std::chrono::steady_clock::now();
+//    auto diff = end - start;
+//    typedef std::chrono::duration<float> float_seconds;
+//    auto secs = std::chrono::duration_cast<float_seconds>(diff);
+//    std::cout << "Time elapsed: " << secs.count() << " s." << std::endl;
+
+    return 0;
+}
+
